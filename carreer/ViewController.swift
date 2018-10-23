90e9b844c8e5fc9ee9f0b3bde92bbd58e5a5847f
nitish nayak n <nageshnitishnnayak@gmail.com>
2018-10-23 10:17:50 +0530

    Initial Commit

---
+++ carreer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  carreer
+//
+//  Created by nitish nayak n on 23/10/18.
+//  Copyright © 2018 nitish nayak n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
