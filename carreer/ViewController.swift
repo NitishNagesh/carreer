107f90d32a697a93e7ef40265be58d943509c14d
nitish nayak n <nageshnitishnnayak@gmail.com>
2018-10-23 11:48:50 +0530

    my idea of implementing

--- carreer/ViewController.swift
+++ carreer/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var customerSupportButton: UIButton!
+    @IBOutlet weak var backendOperationButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        customerSupportButton.isHidden = true
+        backendOperationButton.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func fullTimePressed(_ sender: UIButton) {
+        
+          customerSupportButton.isHidden = !customerSupportButton.isHidden
+        backendOperationButton.isHidden = !backendOperationButton.isHidden
+        }
+  
+    @IBAction func customerSupportpressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func backendOperationPressed(_ sender: UIButton) {
+    }
+    
 }
+
 
